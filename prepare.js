8519d2de5fa909be834807fae6e15126b931d48f
Anton Verinov <git@zemlan.in>
2018-06-26 16:40:57 +0300

    custom app checks in `prepare.js`

--- prepare.js
+++ prepare.js
@@ -1,14 +1,50 @@
-module.exports = function prepare() {
-  const neededApplications = [
-    "Terminal",
-    "Script Editor",
-    "Things",
-    "Sublime Text"
-  ];
+const neededApplications = {
+  Terminal: [],
+  "Script Editor": [],
+  Things: [
+    {
+      check: () =>
+        Application("System Events")
+          .processes.byName("Things")
+          .windows[0].title() === "Shopping",
+      message: `Open "Shopping" project in Things`
+    }
+  ],
+  "Sublime Text": [],
+  Safari: []
+};
 
-  for (const app of neededApplications) {
+module.exports = function prepare() {
+  const se = Application("System Events");
+  se.includeStandardAdditions = true;
+
+  for (const app in neededApplications) {
+    try {
+      Application(app);
+    } catch (e) {
+      console.log(`\u2718`, `${app}`);
+      throw new Error(`Install "${app}"`);
+    }
+
     if (!Application(app).running()) {
+      console.log(`\u2718`, `${app}`);
       throw new Error(`Start "${app}"`);
     }
+
+    if (!se.processes.byName(app).windows.length) {
+      console.log(`\u2718`, `${app}`);
+      throw new Error(`Open window for "${app}"`);
+    }
+
+    const activeAppWindow = se.processes.byName(app).windows[0];
+
+    for (const customCheck of neededApplications[app]) {
+      if (!customCheck.check()) {
+        console.log(`\u2718`, `${app}`);
+        throw new Error(customCheck.message);
+      }
+    }
+
+    console.log("\u2714", app);
   }
 };
