fb42fe775a14eb264c84eb1e6e8b62138835a22d
Anton Verinov <git@zemlan.in>
2018-06-26 16:55:48 +0300

    release-notes screenshot

--- actions/safari-jxa-cookbook.js
+++ actions/safari-jxa-cookbook.js
@@ -1,14 +1,22 @@
 module.exports.name = "JXA Cookbook";
 
+// https://developer.apple.com/library/archive/releasenotes/InterapplicationCommunication/RN-JavaScriptForAutomation/Articles/OSX10-10.html
+const releaseNotesScreenshot = Path("./img/jxa-release-notes.png")
+
 // https://github.com/JXA-Cookbook/JXA-Cookbook/wiki
+const cookbookScreenshot = Path("./img/jxa-cookbook.png")
+
 module.exports.action = function JXACookbook() {
   const Terminal = Application("Terminal");
   const se = Application("System Events");
   Terminal.activate();
-  // se.keystroke("n", {using: "command down"})
-  // delay(4)
-  se.keystroke(`qlmanage -p "${Path("./img/jxa-cookbook.png")}" 2> /dev/null`);
+
+  se.keystroke(
+    `qlmanage -p "${releaseNotesScreenshot}" "${cookbookScreenshot}" 2> /dev/null`
+  );
   se.keyCode(keyCode.ENTER);
-  delay(4);
+  delay(5);
+  se.keyCode(keyCode.RIGHT);
+  delay(5);
   se.keyCode(keyCode.ESC);
 };
