0db23b7771bcd58e781f8d2bd06248c442b35a19
Anton Verinov <git@zemlan.in>
2018-06-26 18:06:26 +0300

    rename safari-jxa-cookbook to docs

--- actions/docs.js
+++ actions/docs.js
@@ -1,15 +1,16 @@
-module.exports.name = "JXA Cookbook";
+module.exports.name = "Docs";
 
 // https://developer.apple.com/library/archive/releasenotes/InterapplicationCommunication/RN-JavaScriptForAutomation/Articles/OSX10-10.html
-const releaseNotesScreenshot = Path("./img/jxa-release-notes.png")
+const releaseNotesScreenshot = Path("./img/jxa-release-notes.png");
 
 // https://github.com/JXA-Cookbook/JXA-Cookbook/wiki
-const cookbookScreenshot = Path("./img/jxa-cookbook.png")
+const cookbookScreenshot = Path("./img/jxa-cookbook.png");
 
-module.exports.action = function JXACookbook() {
+module.exports.action = function docs() {
   const Terminal = Application("Terminal");
   const se = Application("System Events");
   Terminal.activate();
+  delay(0.5);
 
   se.keystroke(
     `qlmanage -p "${releaseNotesScreenshot}" "${cookbookScreenshot}" 2> /dev/null`
